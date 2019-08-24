cdf197200a2b400b4804a3a5550811533051a00f
Roman Podymov <podymfrombryansk@yandex.ru>
2019-08-24 18:17:12 +0200

    Load configuration from sme.yml

--- Sources/SlackMessagesEstimatorUtility/main.swift
+++ Sources/SlackMessagesEstimatorUtility/main.swift
@@ -2,8 +2,9 @@ import Foundation
 import SlackMessagesEstimator
 import Rainbow
 
-if let configurationFilePathURL = URL(string: CommandLine.arguments.first ?? FileManager.default.currentDirectoryPath),
-    let messagesEstimator = SlackMessagesEstimator(
+if let configurationFilePathURL = URL(
+    string: (CommandLine.arguments.first ?? FileManager.default.currentDirectoryPath) + "/sme.yml"
+), let messagesEstimator = SlackMessagesEstimator(
     configurationFilePath: configurationFilePathURL
 ) {
     messagesEstimator.start()
