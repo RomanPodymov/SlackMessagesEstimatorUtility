f1d8c4cd8b0cb9106ad0b7d042e6331d1fd550ff
Roman Podymov <podymfrombryansk@yandex.ru>
2019-08-25 01:45:01 +0200

    Parse arguments

--- Sources/SlackMessagesEstimatorUtility/main.swift
+++ Sources/SlackMessagesEstimatorUtility/main.swift
@@ -1,9 +1,16 @@
 import Foundation
-import SlackMessagesEstimator
 import Rainbow
+import SlackMessagesEstimator
+import SwifterSwift
 
+let configurationFilePath: String
+if CommandLine.argc > 1, let configurationFilePathArg = CommandLine.arguments[safe: 1] {
+    configurationFilePath = configurationFilePathArg
+} else {
+    configurationFilePath = FileManager.default.currentDirectoryPath
+}
 if let configurationFilePathURL = URL(
-    string: (CommandLine.arguments.first ?? FileManager.default.currentDirectoryPath) + "/sme.yml"
+    string: URL(string: configurationFilePath + "/sme.yml")
 ), let messagesEstimator = SlackMessagesEstimator(
     configurationFilePath: configurationFilePathURL
 ) {
